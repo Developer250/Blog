412a954b3cc6abe7b9bef54dad4d6d7d890092e4
Developer250 <jani.kekalainen@hotmail.com>
2023-09-08 17:36:30 +0300

    Jatka the ninja videon 9 jaksosta

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Navbar from './Navbar';
 import Home from './Home';
 
+let name = "Mario";
+
+const handleClick = () => {
+
+}
+
 function App() {
   return (
       <div className="App">
           <Navbar />
           <div className="content">
               <Home />
+              
           </div>
       </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [name, setName] = useState("Mario");
+
+    const handleCLick = () => {
+        setName("Luigi");
+    }
     return (
         <div className="home">
             <h2>Home page</h2>
+            <p> { name }</p>
+            <button onClick={handleCLick}>Click me</button>
         </div>
     );
 }
