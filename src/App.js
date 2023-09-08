92831f4a86fd2ea03e69a2757f752c4793a4a2a4
Developer250 <jani.kekalainen@hotmail.com>
2023-09-08 11:02:42 +0300

    Linkit ja otsikot tehty luotu testausta varten

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from './Navbar';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+          <Navbar />
+          <div className="content">
+              <h1>App component</h1>
+          </div>
+      </div>
+  );
+}
+
+export default App;
