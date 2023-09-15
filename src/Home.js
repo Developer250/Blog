0dd4d4a8c9f7183d68544ba8f245abdc5f5c3f2d
Developer250 <jani.kekalainen@hotmail.com>
2023-09-15 15:02:21 +0300

    error viestin laittaminen näytölle, kun jos dataa ei voi löytää (esim. ei ole tiettyä palvelinta olemassa)

--- src/Home.js
+++ src/Home.js
@@ -8,25 +8,35 @@ const Home = () => {
   //Latausviesti palvelimessa
   const [loading, setLoading] = useState(true);
 
+  const [error, setError] = useState(null);
+
   //Haetaan data/db.json kansion tiedot eli taulukossa oelvat tiedot
+  //If-statementissä tarksitetaan, jos ei olemassa olevaa palvelinta ole, niin lähetetään käyttäjälle virheviesti
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((res) => {
+          if (!res.ok) {
+            throw Error("Could not fetch the data for that resource");
+          }
           return res.json();
         })
         .then((data) => {
           setBlogs(data);
           setLoading(false);
+        })
+        .catch((err) => {
+          setError(err.message);
         });
     }, 400);
   }, []);
 
   // Filtter�id��n blogit toisistaan auktoriteetti-ominaisuuden avulla, jonka arvo on Mario. Lis�ksi m��ritell��n
-  //Testataan useEffect toimintoa, kun halutaan vain, ett� nimi muuttuu, kun klikataan nimi-buttonia,
+  //Testataan useEffect toimintoa, kun halutaan vain, että nimi muuttuu, kun klikataan nimi-buttonia,
   //mutta ei muutu kun klikataan poista - buttonia eli render�id��n yht� asiaa, ei kaikkia.
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {loading && <div> loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs!" />}
     </div>
