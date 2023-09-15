3a311243b36dd5f65209b0d9e5300aae19d297d2
Developer250 <jani.kekalainen@hotmail.com>
2023-09-15 14:32:55 +0300

    loading viesti

--- src/Home.js
+++ src/Home.js
@@ -5,16 +5,21 @@ const Home = () => {
   //Asetetaan blogit taulukkoon oletus arvoihin ja arvot näytölle.
   //setBlogs avulla voidaan muuttaa blogin tilaa eli klikataan jotain ja tapahtuu jotain.
   const [blogs, setBlogs] = useState(null);
+  //Latausviesti palvelimessa
+  const [loading, setLoading] = useState(true);
 
   //Haetaan data/db.json kansion tiedot eli taulukossa oelvat tiedot
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setLoading(false);
+        });
+    }, 300);
   }, []);
 
   // Filtter�id��n blogit toisistaan auktoriteetti-ominaisuuden avulla, jonka arvo on Mario. Lis�ksi m��ritell��n
@@ -22,6 +27,7 @@ const Home = () => {
   //mutta ei muutu kun klikataan poista - buttonia eli render�id��n yht� asiaa, ei kaikkia.
   return (
     <div className="home">
+      {loading && <div> loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs!" />}
     </div>
   );
