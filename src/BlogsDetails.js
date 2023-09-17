e450f33198cee5a6303e0d3368e148f4c71a0163
Developer250 <jani.kekalainen@hotmail.com>
2023-09-17 17:25:22 +0300

    blogdetails näkyy näytöllä, mutta ei blogit homesa

--- src/BlogsDetails.js
+++ src/BlogsDetails.js
@@ -1,9 +1,14 @@
-const blogDetails = () => {
+import { useParams } from "react-router-dom";
+
+const blogsDetails = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { id } = useParams();
+
   return (
     <div className="blog-details">
-      <h2>Blog details</h2>
+      <h2>Blog details - {id}</h2>
     </div>
   );
 };
 
-export default blogDetails;
+export default blogsDetails;
