8f5706230faf451a0550c8378415ca1e6d1a64d5
Developer250 <jani.kekalainen@hotmail.com>
2023-09-19 00:39:53 +0300

    POST päätepiste lisätty uuden blogin lisäämiseksi

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,34 @@
 import { useState } from "react";
+
 //Controlled inputs
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [isLoading, setLoading] = useState(false);
+
+  //Form's submission with page's update prevention when submitting the form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    //POST request to an endpoint
+    setLoading(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-type": "applcation/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setLoading(false);
+    });
+  };
 
   return (
     <div className="create">
       <h2>Add a new blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Title</label>
         <input
           type="text"
@@ -28,10 +48,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="luigi">luigi</option>
         </select>
-        <button>Add a new blog</button>
-        <p>{title} </p>
-        <p>{body} </p>
-        <p>{author} </p>
+        {!isLoading && <button>Add a new blog</button>}
+        {isLoading && <button> disabeld Adding a new blog...</button>}
       </form>
     </div>
   );
