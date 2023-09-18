b7ef810a712caa1aa91bc4a72bb8195e7f3ec459
Developer250 <jani.kekalainen@hotmail.com>
2023-09-18 23:13:41 +0300

    Uuteen blogiin lisätty sisältöä

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,17 @@ const Create = () => {
   return (
     <div className="create">
       <h2>Add a new blog</h2>
+      <form>
+        <label>Title</label>
+        <input type="text" required />
+        <label>Blog body:</label>
+        <textarea required></textarea>
+        <label>Blog Author:</label>
+        <select>
+          <option value="mario">mario</option>
+          <option value="luigi">luigi</option>
+        </select>
+      </form>
     </div>
   );
 };
