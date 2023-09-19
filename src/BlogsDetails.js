8cc3686814d49a92d56b1b6365b441c0f41a36b3
Developer250 <jani.kekalainen@hotmail.com>
2023-09-19 14:50:42 +0300

    korjaa useFetch ja useHistory

--- src/BlogsDetails.js
+++ src/BlogsDetails.js
@@ -1,12 +1,36 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const blogsDetails = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const { id } = useParams();
+  const history = useHistory();
+  const {
+    data: blogs,
+    isLoading,
+    error,
+  } = useFetch("http://localhost:8000/blogs/" + id);
 
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blogs.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
   return (
     <div className="blog-details">
-      <h2>Blog details - {id}</h2>
+      {isLoading && <div> Loading...</div>}
+      {error && <div>{error}...</div>}
+      {blogs && (
+        <article>
+          <h2>{blogs.title} </h2>
+          <h2>{blogs.title} </h2>
+          <p>written by{blogs.author} </p>
+          <div>{blogs.body} </div>
+          <button onClick={handleClick}> delete </button>
+        </article>
+      )}
     </div>
   );
 };
